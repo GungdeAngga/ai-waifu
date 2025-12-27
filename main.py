4df737976e626efa779afdc4028d346a51fe39d0
GungdeAngga <agungangga2001@gmail.com>
2025-12-27 10:33:57 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from waifu import Waifu
+
+def main():
+    waifu = Waifu()
+
+    waifu.initialize(user_input_service='console',
+                     stt_duration = None,
+                     mic_index = None,
+
+                    chatbot_service='openai',
+                    chatbot_model = None,
+                    chatbot_temperature = None,
+                    personality_file = None,
+
+                    tts_service='voicevox', 
+                    output_device=12,
+                    tts_voice='3',
+                    tts_model = None
+                    )
+
+    while True:
+        waifu.conversation_cycle()
+
+if __name__ == "__main__":
+    main()
